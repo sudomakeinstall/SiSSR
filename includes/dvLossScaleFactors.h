34c1faff34833e45fe546a032b92d4333c776afb
Davis Vigneault <davis.vigneault@gmail.com>
2019-07-22 00:08:57 -0700

    Use the defaults specified in the original MedIA paper

--- includes/dvLossScaleFactors.h
+++ includes/dvLossScaleFactors.h
@@ -6,6 +6,10 @@ namespace dv
 
 struct LossScaleFactors
 {
+  // Use defaults specified in the paper:
+  // SiSSR: Simultaneous subdivision surface registration for the
+  // quantification of cardiac function from computed tomography in canines.
+  // https://www.ncbi.nlm.nih.gov/pubmed/29627686
   LossScaleFactors(const double _Robust,
                    const double _Velocity,
                    const double _Acceleration,
@@ -21,9 +25,9 @@ struct LossScaleFactors
   LossScaleFactors() :
     Robust(1.0),
     Velocity(0.0),
-    Acceleration(0.0),
-    ThinPlate(0.0),
-    TriangleAspectRatio(0.0),
+    Acceleration(0.1),
+    ThinPlate(0.1),
+    TriangleAspectRatio(10.0),
     EdgeLength(0.0) {};
   double Robust;
   double Velocity;
