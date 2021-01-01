d127a6680312187bd698b06f27f5b5461c9f9839
Davis Vigneault <davis.vigneault@gmail.com>
2021-01-01 11:04:49 -0800

    BUG: Add missing header tests

---
+++ test/src/itkCleanSegmentationImageFilterHeaderTest.cxx
@@ -0,0 +1,5 @@
+#include <itkCleanSegmentationImageFilter.h>
+#include <cstdlib>
+int main() {
+  return EXIT_SUCCESS;
+}
---
+++ test/src/sissrEdge_preserving_midpoint_placementHeaderTest.cxx
@@ -0,0 +1,5 @@
+#include <sissrEdge_preserving_midpoint_placement.h>
+#include <cstdlib>
+int main() {
+  return EXIT_SUCCESS;
+}
