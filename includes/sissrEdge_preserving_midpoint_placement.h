c6b835101391a800e906fdce70ca6e15f04d3286
Davis Marc Vigneault <davis.vigneault@gmail.com>
2021-02-23 14:10:38 -0800

    BUG: Preserve topology

--- includes/sissrEdge_preserving_midpoint_placement.h
+++ includes/sissrEdge_preserving_midpoint_placement.h
@@ -11,69 +11,62 @@ template<class TM_, typename TData>
 class Edge_preserving_midpoint_placement
 {
 public:
-  typedef TM_                                                     TM;
+  typedef TM_ TM;
 
   Edge_preserving_midpoint_placement() {}
+
+  template <typename Profile>
+  bool labels_uniform_around_vertex(const Profile& profile, const typename Profile::halfedge_descriptor &edge) const {
+
+    const auto property = std::get<0>(profile.surface_mesh().template property_map<typename Profile::Triangle_mesh::Face_index,TData>("f:data"));
+
+    bool uniform_around_vertex = true;
+
+    typename Profile::halfedge_descriptor i = edge;
+
+    const auto f = profile.surface_mesh().face(edge);
+    const auto d = property[f];
+
+    do {
+      i = profile.surface_mesh().next_around_source(i);
+      const auto fn = profile.surface_mesh().face(i);
+      const auto dn = property[fn];
+      if (d != dn) {
+        uniform_around_vertex = false;
+        break;
+      }
+    } while (i != edge);
+
+    return uniform_around_vertex;
+
+  }
 
   template <typename Profile>
   boost::optional<typename Profile::Point> operator()(const Profile& profile) const
   {
     const auto property = std::get<0>(profile.surface_mesh().template property_map<typename Profile::Triangle_mesh::Face_index,TData>("f:data"));
 
-    bool uniform_around_source = true;
-
-    {
-      // Iterate around the source vertex and determine
-      // whether the faces surrounding the source vertex
-      // all share the same data value.
-      const auto f = profile.surface_mesh().face(profile.v0_v1());
-      const auto d = property[f];
-      typename Profile::halfedge_descriptor i = profile.v0_v1();
-      do {
-        i = profile.surface_mesh().next_around_source(i);
-        const auto fn = profile.surface_mesh().face(i);
-        const auto dn = property[fn];
-        if (d != dn) {
-          uniform_around_source = false;
-          break;
-        }
-      } while (i != profile.v0_v1());
-    }
-
-    bool uniform_around_target = true;
-
-    {
-      // Iterate around the source vertex and determine
-      // whether the faces surrounding the source vertex
-      // all share the same data value.
-      const auto f = profile.surface_mesh().face(profile.v1_v0());
-      const auto d = property[f];
-      typename Profile::halfedge_descriptor i = profile.v1_v0();
-      do {
-        i = profile.surface_mesh().next_around_source(i);
-        const auto fn = profile.surface_mesh().face(i);
-        const auto dn = property[fn];
-        if (d != dn) {
-          uniform_around_target = false;
-          break;
-        }
-      } while (i != profile.v1_v0());
-    }
+    bool uniform_around_source = this->labels_uniform_around_vertex<Profile>(profile, profile.v0_v1());
+    bool uniform_around_target = this->labels_uniform_around_vertex<Profile>(profile, profile.v1_v0());
 
     typedef boost::optional<typename Profile::Point>              result_type;
 
-    if (uniform_around_source == uniform_around_target) {
+    if (uniform_around_source && uniform_around_target) {
       return result_type(profile.geom_traits().construct_midpoint_3_object()(profile.p0(), profile.p1()));
     } else if (uniform_around_source) {
       return result_type(profile.p1());
-    } else {
+    } else if (uniform_around_target) {
       return result_type(profile.p0());
+    } else {
+      return result_type();
     }
 
   }
+
 };
 
 }
+
 }
 
 #endif
