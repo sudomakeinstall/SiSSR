db4853578dc52254d2988ed6e20e70449d30ac81
Davis Vigneault <davis.vigneault@gmail.com>
2021-02-24 21:44:21 -0800

    ENH: Preserve edge placement

--- includes/sissrEdge_preserving_midpoint_placement.h
+++ includes/sissrEdge_preserving_midpoint_placement.h
@@ -44,20 +44,32 @@ public:
   {
     const auto property = std::get<0>(profile.surface_mesh().template property_map<typename Profile::Triangle_mesh::Face_index,TData>("f:data"));
 
+    const auto data_l = property[profile.surface_mesh().face(profile.v0_v1())];
+    const auto data_r = property[profile.surface_mesh().face(profile.v1_v0())];
+    bool edge_lies_on_boundary = (data_l != data_r);
     bool uniform_around_source = this->labels_uniform_around_vertex<Profile>(profile, profile.v0_v1());
     bool uniform_around_target = this->labels_uniform_around_vertex<Profile>(profile, profile.v1_v0());
+    bool edge_is_internal = (uniform_around_source && uniform_around_target);
 
     using result_type = boost::optional<typename Profile::Point>;
 
-    if (uniform_around_source && uniform_around_target) {
+    if (edge_is_internal || edge_lies_on_boundary) {
+      // If the edge is entirely internal, or if the edge
+      // lies on a boundary, it may be contracted to the midpoint.
       return result_type(profile.geom_traits().construct_midpoint_3_object()(profile.p0(), profile.p1()));
     } else if (uniform_around_source) {
+      // If only the target lies on the boundary,
+      // it should be contracted to the target.
       return result_type(profile.p1());
     } else if (uniform_around_target) {
+      // If only the source lies on the boundary,
+      // it should be contracted to the source.
       return result_type(profile.p0());
     } else {
-      return result_type(profile.p0());
-//      return result_type();
+      // In this case, the source and target lie on different
+      // boundaries (connecting two separated components).
+      // In this case, the edge should not be contracted.
+      return result_type();
     }
 
   }
