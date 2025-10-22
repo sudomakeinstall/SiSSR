7ecb9cc05e2c2fd70874198dbd2b0c058fba8d05
Davis Vigneault <davis.vigneault@gmail.com>
2025-10-21 19:52:57 -0700

    Add run options

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-# Build the dv-sissr Docker image
-docker build -t dv-sissr:latest .
-
-# Test the build by running help
-echo "Testing Docker build..."
-docker run --rm dv-sissr:latest --help
+case "$1" in
+  build)
+    echo "Building dv-sissr Docker image..."
+    docker build -t dv-sissr:latest .
+    ;;
+  test)
+    echo "Testing Docker build..."
+    docker run --rm dv-sissr:latest --help
+    ;;
+  help)
+    echo "Usage: $0 {build|test|help}"
+    echo "  build - Build the dv-sissr Docker image"
+    echo "  test  - Test the Docker image by running --help"
+    echo "  help  - Show this help message"
+    ;;
+  *)
+    echo "Usage: $0 {build|test|help}"
+    exit 1
+    ;;
+esac
